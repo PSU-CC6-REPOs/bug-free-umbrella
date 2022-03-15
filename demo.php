19da78136702dfdc8d6493dcec965f8a16caa768
dianarod28 <201880207@psu.palawan.edu.ph>
2022-03-15 08:54:46 +0800

    Changes has been made

--- demo.php
+++ demo.php
@@ -1,5 +1,17 @@
 <?php
 
-$sentence='The quick brown fox jumps over the lazy dog';
-echo implode('', array_slice(explode('',$sentence),2,9))."\n";
+$sentence=array("the","quick","brown","fox","jumps","over","the","lazy","dog");
+
+foreach($sentence as $x)
+{
+    if($x == 'the'){
+        continue;
+    }else{
+        echo $x.' ';
+    }
+}
+
+//$sentence='The quick brown fox jumps over the lazy dog';
+//echo implode('', array_slice(explode('',$sentence),2,9))."\n";
 ?>
+
